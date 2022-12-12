cd738a89a9b25413c5dc13dfe93e9028b595acb8
Kenjie <kbalona5@gmail.com>
2022-12-12 11:26:08 +0800

    commit all

--- src/CircleGUI.java
+++ src/CircleGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CircleGUI extends JFrame{
     private JPanel pnlMain;
@@ -8,11 +10,43 @@ public class CircleGUI extends JFrame{
     private JButton btnClear;
     private JButton btnCompute;
 
+    public CircleGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                computed();
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clear();
+            }
+        });
+    }
+
+    public void computed() {
+        String r = tfRadius.getText();
+        double a = (double) (Math.pow(Integer.parseInt(r), 2) * Math.PI);
+        tfArea.setText(String.valueOf(a));
+        double c = (double) (Integer.parseInt(r) * Math.PI) * 2;
+        tfCircumference.setText(String.valueOf(c));
+    }
+
+    public void clear() {
+        tfArea.setText("");
+        tfCircumference.setText("");
+    }
+
+
     public static void main(String[] args) {
         CircleGUI app = new CircleGUI();
         app.setContentPane(app.pnlMain);
         app.setSize(700, 700);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+
     }
+
 }
